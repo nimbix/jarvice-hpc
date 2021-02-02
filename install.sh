5aea45ce6c023c03edce16fabccd59c0caacc9f5
Kenneth Hill <ken.hill@nimbix.net>
2021-02-02 13:30:28 -0600

    Update build
    Signed-off-by: Kenneth Hill <ken.hill@nimbix.net>

--- install.sh
+++ install.sh
@@ -13,11 +13,13 @@ cp *.go $CLIENT
 
 docker run -ti --rm -v "$PWD":/usr/src/jarvice-hpc \
     -w /usr/src/jarvice-hpc \
-    -e GOOS=linux golang:1.14 \
+    -e GOOS=linux \
+    -e CGO_ENABLED=0 \
+    golang:1.14 \
     /bin/bash -c "go get github.com/jessevdk/go-flags \
     && mkdir -p /go/src/jarvice.io \
     && ln -s /usr/src/jarvice-hpc/core /go/src/jarvice.io \
-    && go build -v -o jarvice $CLIENT/*.go"
+    && go build -v -o jarvice -a -ldflags '-extldflags -static -s -w' $CLIENT/*.go"
 
 for file in `ls *.go`; do
     rm -f $CLIENT/$file
